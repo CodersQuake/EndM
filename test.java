3c1653d8b269776a5b14bf4391f2059a6df71661
CodersQuake <79209038+CodersQuake@users.noreply.github.com>
2025-01-09 12:23:31 +0530

    Created the file test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+
+public class Addition
+{
+  public static void main(String[] args)
+  {
+    int a = 100;
+    int b = 200;
+
+    System.out.println("The Addition Of Two Numbers Are : " + (a+b));
+  }
+}
